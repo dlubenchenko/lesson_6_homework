cbd152b274fdb21526cefb175a0535ba503f7205
Lubenchenko Dmytro <d.lubenchenko@gmail.com>
2016-11-08 16:16:06 +0000

    first commit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 // JavaScript File
 //Math.pow(base, exponent)
-    
+
     var a = Number(prompt('Enter a'));
     var b = Number(prompt('Enter b'));
     var c = Number(prompt('Enter c'));
-    var x = 'x';
-    var x1 = 'x1';
-    var x2 = 'x2';
-    
 
+    var sqrtPov = (Math.pow(b, 2)) - 4 * a * c;
+    console.log(sqrtPov);
+    var sqrtD = Math.sqrt(sqrtPov);
+    console.log(sqrtD);
 
-function mathPow(a, b, c, x, x1, x2) {
-    var mathPow = a * Math.pow(x, 2) + b*x + c;
-    var sqrtD = Math.pow(b, 2) + (4 * a + c);
-    var mathSqrt1 = ( -b + Math.sqrt(sqrtD))/ 2 * a;
-    var mathSqrt2 = ( -b - Math.sqrt(sqrtD))/ 2 * a;
-
-
-}
-document.write(sqrtD);
-document.write(mathSqrt1);
+    if (sqrtD > 0) {
+        b=-b;
+        var x1 = (b + sqrtD) / (2 * a);
+        document.write('D > 0 / ',x1);
+        var x2 = (b - sqrtD) / (2 * a);
+        document.write(' D > 0 / ',x2);
+    }  else {
+        var x3 = - b / (2 * a);
+        var x4 = - b / (2 * a);
+        document.write('0 / ', x2, x3);
+    }
